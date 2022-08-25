40b36baa28561bfdd68480ff3e64d9631b155e95
danyouknowme <thanathip.suw@gmail.com>
2022-08-25 22:29:28 +0700

    fixed warning context

--- pkg/database/connect.go
+++ pkg/database/connect.go
@@ -20,12 +20,14 @@ func ConnectDB() *mongo.Client {
 		log.Fatal(err)
 	}
 
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	err = client.Connect(ctx)
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(3)
 	}
+	defer cancel()
+
 	fmt.Println("Connect to MongoDB database successfully!")
 	return client
 }
