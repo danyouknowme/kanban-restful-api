e74525f5564846f6ee602098189745da536dbecb
danyouknowme <thanathip.suw@gmail.com>
2022-08-26 18:26:21 +0700

    added api edit task list same column

--- pkg/api/task.go
+++ pkg/api/task.go
@@ -74,10 +74,103 @@ func CreateTask() gin.HandlerFunc {
 	}
 }
 
-// func EditTaskStatus() gin.HandlerFunc {
-// 	return func(c *gin.Context) {
-// 		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+func EditTaskListSameColumn() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		var req models.EditTaskListSameColumnRequest
+		userId := c.Query("userId")
+		boardId := c.Query("boardId")
+		defer cancel()
 
-// 		defer cancel()
-// 	}
-// }
+		if err := c.BindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, err.Error())
+			return
+		}
+
+		board, err := GetBoardById(ctx, boardId)
+		if err != nil {
+			c.JSON(http.StatusNotFound, err.Error())
+			return
+		}
+
+		if board.UserId != userId {
+			c.JSON(http.StatusForbidden, gin.H{"message": "you don't allow to do this"})
+			return
+		}
+
+		boardTask := board.BoardTask[req.BoardTaskId]
+		boardTask.TaskList = req.TaskList
+		board.BoardTask[req.BoardTaskId] = boardTask
+
+		updatedBoardTask := bson.M{
+			"$set": bson.M{
+				"board_task": board.BoardTask,
+			},
+		}
+
+		_, err = boardCollection.UpdateOne(
+			ctx,
+			bson.M{"_id": board.Id},
+			updatedBoardTask,
+		)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		c.JSON(http.StatusOK, board)
+	}
+}
+
+func EditTaskListDifferentColumn() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		var req models.EditTaskListDifferentColumnRequest
+		userId := c.Query("userId")
+		boardId := c.Query("boardId")
+		defer cancel()
+
+		if err := c.BindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, err.Error())
+			return
+		}
+
+		board, err := GetBoardById(ctx, boardId)
+		if err != nil {
+			c.JSON(http.StatusNotFound, err.Error())
+			return
+		}
+
+		if board.UserId != userId {
+			c.JSON(http.StatusForbidden, gin.H{"message": "you don't allow to do this"})
+			return
+		}
+
+		sourceBoardTask := board.BoardTask[req.SourceId]
+		destBoardTask := board.BoardTask[req.DestinationId]
+
+		sourceBoardTask.TaskList = req.SourceItems
+		destBoardTask.TaskList = req.DestinationItems
+
+		board.BoardTask[req.SourceId] = sourceBoardTask
+		board.BoardTask[req.DestinationId] = destBoardTask
+
+		updatedBoardTask := bson.M{
+			"$set": bson.M{
+				"board_task": board.BoardTask,
+			},
+		}
+
+		_, err = boardCollection.UpdateOne(
+			ctx,
+			bson.M{"_id": board.Id},
+			updatedBoardTask,
+		)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		c.JSON(http.StatusOK, board)
+	}
+}
